26a20a8731ce23a70f292776d66a0f946687b94c
Carlo Aguirre <111251496+CarloAguirre@users.noreply.github.com>
2026-07-27 20:07:46 -0400

    Registra resolver de catálogos históricos

--- apps/api/src/modules/inspection-legacy-import/inspection-legacy-import.module.ts
+++ apps/api/src/modules/inspection-legacy-import/inspection-legacy-import.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AreaEntity } from '../organization/entities/area.entity';
+import { CompanyEntity } from '../organization/entities/company.entity';
+import { UserEntity } from '../users/entities/user.entity';
 import { InspectionLegacyImportEntity } from './entities/inspection-legacy-import.entity';
 import { InspectionLegacyMilestoneEntity } from './entities/inspection-legacy-milestone.entity';
 import { InspectionLegacyNormalizerService } from './inspection-legacy-normalizer.service';
+import { InspectionLegacyResolverService } from './inspection-legacy-resolver.service';
 import { InspectionLegacySourceManifestService } from './inspection-legacy-source-manifest.service';
 
 @Module({
@@ -10,15 +14,20 @@ import { InspectionLegacySourceManifestService } from './inspection-legacy-sourc
     TypeOrmModule.forFeature([
       InspectionLegacyImportEntity,
       InspectionLegacyMilestoneEntity,
+      AreaEntity,
+      CompanyEntity,
+      UserEntity,
     ]),
   ],
   providers: [
     InspectionLegacyNormalizerService,
+    InspectionLegacyResolverService,
     InspectionLegacySourceManifestService,
   ],
   exports: [
     TypeOrmModule,
     InspectionLegacyNormalizerService,
+    InspectionLegacyResolverService,
     InspectionLegacySourceManifestService,
   ],
 })
